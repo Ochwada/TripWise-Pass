e8702c7105a1b2e931034adce42042857a36317d
Linda Ochwada <nakhulolinda@yahoo.com>
2025-08-21 11:03:13 +0200

    projec done (localhost)

--- src/main/java/com/tripwise/trippass/controller/AuthApplication.java
+++ src/main/java/com/tripwise/trippass/controller/AuthApplication.java
@@ -5,7 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.security.oauth2.jwt.Jwt;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Map;
 
 /**
  * ================================================================
