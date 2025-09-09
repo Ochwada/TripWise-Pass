b1a63bf3498081413f3749fcd96521c64dc1eb86
Linda Ochwada <nakhulolinda@yahoo.com>
2025-09-09 14:16:52 +0200

    added the cors

--- src/main/java/com/tripwise/trippass/config/CorsConfig.java
+++ src/main/java/com/tripwise/trippass/config/CorsConfig.java
@@ -3,6 +3,9 @@ package com.tripwise.trippass.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.*;
+
+
 /**
  * ================================================================
  * Package Name: com.tripwise.trippass.config
@@ -17,9 +20,9 @@ public class CorsConfig {
     @Bean
     public org.springframework.web.cors.CorsConfigurationSource corsConfigurationSource() {
         var cfg = new org.springframework.web.cors.CorsConfiguration();
-        cfg.setAllowedOrigins(java.util.List.of("http://localhost:9094"));
-        cfg.setAllowedMethods(java.util.List.of("GET","OPTIONS"));
-        cfg.setAllowedHeaders(java.util.List.of("Content-Type","Authorization"));
+        cfg.setAllowedOrigins(List.of("http://localhost:9094", "http://localhost:9095"));
+        cfg.setAllowedMethods(List.of("GET","OPTIONS"));
+        cfg.setAllowedHeaders(List.of("Content-Type","Authorization"));
         cfg.setAllowCredentials(true); // only if you need cookies
 
         var src = new org.springframework.web.cors.UrlBasedCorsConfigurationSource();
